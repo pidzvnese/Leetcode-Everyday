0732df33ee05076db0c9f5c08e98de9314070f2d
Phong Nguyen <phongnhh92@gmail.com>
2024-06-27 15:31:56 +0700

    Time: 19 ms (89.98%), Space: 19.7 MB (89.38%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.php
@@ -0,0 +1,13 @@
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        if($x == strrev($x)) {
+            return true;
+        }
+        return false;
+    }
+}
